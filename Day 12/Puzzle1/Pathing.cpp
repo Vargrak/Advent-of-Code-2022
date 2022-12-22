90b792f7d31f92d74e47667f9af6c699dd9cbcc7
Vargrak <maxwellernst@protonmail.ch>
2022-12-22 08:31:45 -0500

    pathing initial

--- Day 12/Puzzle1/Pathing.cpp
+++ Day 12/Puzzle1/Pathing.cpp
@@ -10,31 +10,6 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
 
-//Node class
-class Node
-{
-    public:
-    char elevation; //Cost to traverse path
-    std::vector<Node*> connections; //Linking to other paths
-
-    int get_weight_to_other_node(char elevation)
-    {
-        
-    }
-
-    bool is_traverseable(char elevation)
-    {
-        if (elevation == 'X')
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-};
-
 //Gets the file with weighted nodes and returns them as a vector of strings so it's 2d parseable
 std::vector<std::string> get_map_vector()
 {
@@ -49,10 +24,6 @@ std::vector<std::string> get_map_vector()
     return map_vector;
 }
 
-//Parses the map vector at coords x,y and returns a node with neighboring connections 
-Node get_node(std::vector<std::string> map, int x, int y)
-{
 
-}
 
 
