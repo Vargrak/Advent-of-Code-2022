0a505c1fa8a7162d87adce23ecc4bd0c413a30ba
Vargrak <Maxwellernst@protonmail.ch>
2023-01-17 15:49:29 -0500

    aaa

--- Day 15/beacon.cpp
+++ Day 15/beacon.cpp
@@ -11,7 +11,7 @@ int main()
 {
     Map map = Map(load_file("input.txt"));
 
-    std::cout << "Part 1: " << map.get_positions_without_beacons_along_y(2000000) << std::endl;
+    std::cout << "Part 1: " << map.get_positions_in_range_along_y(2000000) << std::endl;
 
     std::cout << "Part 2: " << std::setprecision(30) << map.tuning_frequency(0, 4000000) << std::endl;
 
